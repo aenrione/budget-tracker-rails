c095d82daa3abfb4ebfd96a5e8b5cfbe26d3d7d4
aenrione <aenrione@protonmail.com>
2022-05-11 18:08:25 -0400

    improve user_balance_to_chart

--- app/commands/user_balance_to_chart.rb
+++ app/commands/user_balance_to_chart.rb
@@ -1,13 +1,12 @@
 class UserBalanceToChart < PowerTypes::Command.new(:user)
   def perform
-    result = { labels: [], data: [] }
+    result = { labels: [], data: [], dates: [], formated_data: [] }
     @versions = @user.versions
     @versions.each do |version|
       obj = version.reify
-      # next if obj.balance.zero?
-
-      result[:labels] << obj.updated_at.to_date.to_s
+      result[:dates] << obj.updated_at.to_date.to_s
       result[:data] << obj.balance.to_i
+      result[:formated_data] << obj.balance_amount.format
     end
     result
   end
