d83060f51afbc55c4ce0fdf6f66de510c0b681cc
aenrione <aenrione@protonmail.com>
2022-06-25 16:50:59 -0400

    fix capabilities xd

--- app/commands/get_user_capabilities.rb
+++ app/commands/get_user_capabilities.rb
@@ -1,12 +1,25 @@
 class GetUserCapabilities < PowerTypes::Command.new(:user)
   def perform
-    {
+    result = {
       hasFintocAccount: @user.fintoc_account.present?,
       hasBudaAccount: @user.buda_account.present?,
-      hasFintualAccount: @user.fintual_account.present?,
-      fintoc: Api::V1::FintocAccountSerializer.new(@user.fintoc_account).serializable_hash,
-      buda: Api::V1::BudaAccountSerializer.new(@user.buda_account).serializable_hash,
-      fintual: Api::V1::FintualAccountSerializer.new(@user.fintual_account).serializable_hash
+      hasFintualAccount: @user.fintual_account.present?
     }
+    if @user.fintoc_account.present?
+      result[:fintoc] = Api::V1::FintocAccountSerializer.new(
+        @user.fintoc_account
+      ).serializable_hash
+    end
+    if @user.buda_account.present?
+      result[:buda] = Api::V1::BudaAccountSerializer.new(
+        @user.buda_account
+      ).serializable_hash
+    end
+    if @user.fintual_account.present?
+      result[:fintual] = Api::V1::FintualAccountSerializer.new(
+        @user.fintual_account
+      ).serializable_hash
+    end
+    result
   end
 end
